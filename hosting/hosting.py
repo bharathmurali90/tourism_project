8cec3519fca5dec8426c69b5609464a670aeb341
bharathmurali90 <bharathmuralidharan0390@gmail.com>
2026-02-08 14:14:34 +0530

    changed the hosting repo

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -1,10 +1,30 @@
-from huggingface_hub import HfApi
+from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
+from huggingface_hub import HfApi, create_repo
 import os
 
-api = HfApi(token=os.getenv("HF_TOKEN"))
+
+repo_id="BharathonAI/tourism-newplan-adoption-prediction"        # the target repo
+repo_type="space"
+
+# api = HfApi(token=os.getenv("HF_TOKEN"))
+# api.upload_folder(
+#     folder_path="deployment",     # the local folder containing your files
+#     repo_id="BharathonAI/tourism-newplan-adoption-prediction",          # the target repo
+#     repo_type="space",                      # dataset, model, or space
+#     path_in_repo="",                          # optional: subfolder path inside the repo
+# )
+
+try:
+    api = HfApi(token=os.getenv("HF_TOKEN"))
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
+    print(f"Space '{repo_id}' already exists. Using it.")
+except RepositoryNotFoundError:
+    print(f"Space '{repo_id}' not found. Creating new space...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    print(f"Space '{repo_id}' created.")
+
 api.upload_folder(
-    folder_path="deployment",     # the local folder containing your files
-    repo_id="BharathonAI/tourism-newplan-adoption-prediction",          # the target repo
-    repo_type="space",                      # dataset, model, or space
-    path_in_repo="",                          # optional: subfolder path inside the repo
+    folder_path="deployment",
+    repo_id=repo_id,
+    repo_type=repo_type,
 )
