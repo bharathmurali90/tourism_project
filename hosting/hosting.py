7dcbfb2ef47fa496c909e9c6940533851807404a
bharathmurali90 <bharathmuralidharan0390@gmail.com>
2026-02-08 14:43:13 +0530

    hosting file change

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -18,11 +18,11 @@ private=False  # Make it private if needed
 
 try:
     api = HfApi(token=os.getenv("HF_TOKEN"))
-    api.repo_info(repo_id=repo_id, repo_type=repo_type,space_sdk=space_sdk,private =private)
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
     print(f"Space '{repo_id}' already exists. Using it.")
 except RepositoryNotFoundError:
     print(f"Space '{repo_id}' not found. Creating new space...")
-    create_repo(repo_id=repo_id, repo_type=repo_type, space_sdk=space_sdk,private =private)
+    create_repo(repo_id=repo_id, repo_type=repo_type,space_sdk=space_sdk,private =private)
     print(f"Space '{repo_id}' created.")
 
 api.upload_folder(
